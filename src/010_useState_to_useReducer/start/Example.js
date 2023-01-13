617f0bd280ba83faf1f21eae60bf79c4d82bdb4f
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-13 21:13:43 +0900

    ✨useReducer実装

--- src/010_useState_to_useReducer/start/Example.js
+++ src/010_useState_to_useReducer/start/Example.js
@@ -1,7 +1,32 @@
+import { useReducer, useState } from "react";
+
 const Example = () => {
-  
+  const [state, setState] = useState(0);
+  const countUp = () => {
+    setState((prev) => ++prev);
+  };
+
+  const [rstate, dispatch] = useReducer((prev, action) => {
+    if (action === "+") {
+      return ++prev;
+    } else if (action === "-") {
+      return --prev;
+    }
+  }, 0);
+  const rcountUp = () => {
+    dispatch("+");
+  };
+  const rcountDown = () => {
+    dispatch("-");
+  };
+
   return (
     <>
+      <h3>{state}</h3>
+      <button onClick={countUp}>+</button>
+      <h3>{rstate}</h3>
+      <button onClick={rcountUp}>+</button>
+      <button onClick={rcountDown}>-</button>
     </>
   );
 };
