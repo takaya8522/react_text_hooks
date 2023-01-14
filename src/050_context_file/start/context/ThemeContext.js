b57cca81bb422897c26c76fcaaa7ff4543339352
takaya suzuki <suzuki8522@yahoo.co.jp>
2023-01-15 01:02:25 +0900

    ✨練習問題リファクタリング

---
+++ src/050_context_file/start/context/ThemeContext.js
@@ -0,0 +1,16 @@
+import { useState, useContext, createContext } from "react";
+// import { ThemeContext } from "../Example";
+
+export const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState("light");
+
+  return (
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
